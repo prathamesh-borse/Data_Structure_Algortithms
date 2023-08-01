54f810339d6cc2b4a443e722eca09d9af22d1547
Prathamesh Borse <prathameshborse.official@gmail.com>
2023-08-01 23:23:11 +0530

    Something Changed in Hello Java File

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-package Java;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello and Welcome");
     }
 }
